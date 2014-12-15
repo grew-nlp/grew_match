cd7af0bfd4b7a9ff8b243ff4bfb8663dced203b1
achemard <achemard@7838e531-6607-4d57-9587-6c381814729c>
2014-12-15 15:25:54 +0000

    Mise à jour avant de deployer sur le serveur
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/semagramme/grew_web@8512 7838e531-6607-4d57-9587-6c381814729c

--- ajaxGrew.php
+++ ajaxGrew.php
@@ -1,21 +1,25 @@
 <?php
 	if (isset($_POST['pattern'])) {
 		session_start();
-		error_log(session_id());
-		$historyFile = fopen("data/sessions/" + session_id(),"w");
-		fwrite($historyFile, $_POST['pattern']); 
-		fclose($historyFile);
+		
+		//Historique
+		// $hFile = "data/sessions/";
+		// $hFile .=  (string)session_id();
+		// $historyFile = fopen($hFile,"w");
+		// fwrite($historyFile, $_POST['pattern'] . "\n<+>\n"); 
+		// fclose($historyFile);
 
+		//Création du dossier de données et écriture du pattern dans le dossier correspondant
 		$dir = "data";
-		$count = count(scandir($dir)) - 2;
-		$id = $count + 1;
-
-
-		mkdir("/tmp/777");
-		$pattern = fopen("/tmp/777/pattern","w");
+		$id = uniqid();
+		$old = umask(0);
+		mkdir("data/" . $id,0777);
+		umask($old);
+		$pattern = fopen("data/" . $id . "/pattern","w");
 		fwrite($pattern, $_POST['pattern']); 
 		fclose($pattern);
-		//ligne d'execution
+		
+		//Comunication avec l'application via le port 8080
 		$addr = gethostbyname("localhost");
 		$client = stream_socket_client("tcp://$addr:8181", $errno, $errorMessage);
 
@@ -23,9 +27,21 @@
     		throw new UnexpectedValueException("Failed to connect: $errorMessage");
 		}
 
-		fwrite($client, "777#NEW");
+		fwrite($client, "/opt/lampp/htdocs/grew/data/" . $id . "#NEW");
 		$result = stream_get_contents($client);
 		fclose($client);
-		echo $result;
+		echo $id;
+	}elseif (isset($_POST['id'])) {
+		$addr = gethostbyname("localhost");
+		$client = stream_socket_client("tcp://$addr:8181", $errno, $errorMessage);
+
+		if ($client === false) {
+    		throw new UnexpectedValueException("Failed to connect: $errorMessage");
+		}
+
+		fwrite($client, "/opt/lampp/htdocs/grew/data/" . $_POST['id'] . "#NEXT");
+		$result = stream_get_contents($client);
+		fclose($client);
+		echo false;
 	}
 ?>
--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 var id = "";
 var resultsNumber = 0;
 var watcher = undefined;
-
+var line = 0;
 function request_pattern(){
 	$('#results').show();
 	//Reset de la liste 
 	$('#submit-pattern').prop('disabled',true);
 	$('#list-results').empty();
+	$('#progress-txt').empty();
+	$("#next-results").hide();
 	resultsNumber = 0;
 
 	$.ajax({url:'ajaxGrew.php',
@@ -15,7 +17,6 @@ function request_pattern(){
 		type: 'post',
 		success: function(output){
 			id = output;
-			id = 1;
 			var file = "./data/" + id + "/list";
 
 			var previous = "";
@@ -25,21 +26,108 @@ function request_pattern(){
     			ajax.onreadystatechange = function() {
         			if (ajax.readyState == 4) {
             			if (ajax.responseText != previous) {
+            				var progression = 0;
            					var lines = ajax.responseText.split("\n");
+           					line = 0;
 							for (var i = resultsNumber,len = lines.length; i < len;i++) {
-								
+								line++;
 								if (lines[i] == '<#>') {
 									clearInterval(watcher);
 									watcher = undefined;
 									$('#submit-pattern').prop('disabled',false);
 									i= lines.length;
+									$("#next-results").hide();
+									$('#progress-txt').text('100% du corpus parcourus');
+								}else if (lines[i] == '<!>'){
+									clearInterval(watcher);
+									watcher = undefined;
+									$.get('./data/' + id + '/error',function(errors){
+										sweetAlert("Une erreur est survenue", errors, "error");
+									});
+									i= lines.length;
+									$('#submit-pattern').prop('disabled',false);
+									$("#next-results").hide();
+								}else if (lines[i] == '<?>'){
+									progression = 1;
+								}else if (progression == 1){
+									clearInterval(watcher);
+									$('#submit-pattern').prop('disabled',false);
+									$("#next-results").show();
+									$('#progress-txt').text(lines[i] + '% du corpus parcourus');
+									i = lines.length;
 								}else{
 									$("#list-results").append('<li id="list-' + i + '"><a href="#" >' +  lines[i] + '</a></li>');
 									url = './data/' + id + '/' + lines[i];
 									$('#list-' + i).click({url:url,i:i},display_picture);
 								}
 							};
-							resultsNumber = lines.length;
+							resultsNumber = line;
+                			previous = ajax.responseText;
+            			}
+        			}
+    			};
+    			ajax.open("POST", "./data/" + id + "/list", true); //Use POST to avoid caching
+    			ajax.send();
+			}, 1000);
+		}
+	});
+}
+
+function next_results(){
+	//Reset de la liste 
+	$('#submit-pattern').prop('disabled',true);
+	$("#next-results").hide();
+
+	$.ajax({url:'ajaxGrew.php',
+		dataType:'text',
+		data: {id: id},
+		type: 'post',
+		success: function(output){
+			var file = "./data/" + id + "/list";
+
+			var previous = "";
+
+			watcher = setInterval(function() {
+    			var ajax = new XMLHttpRequest();
+    			ajax.onreadystatechange = function() {
+        			if (ajax.readyState == 4) {
+            			if (ajax.responseText != previous) {
+            				var progression = 0;
+           					var lines = ajax.responseText.split("\n");
+           					
+							for (var i = resultsNumber,len = lines.length; i < len;i++) {
+								line++;
+								if (lines[i] == '<#>') {
+									clearInterval(watcher);
+									watcher = undefined;
+									$('#submit-pattern').prop('disabled',false);
+									$("#next-results").hide();
+									i= lines.length;
+									$('#progress-txt').text('100% du corpus parcourus');
+								}else if (lines[i] == '<!>'){
+									clearInterval(watcher);
+									watcher = undefined;
+									$.get('./data/' + id + '/error',function(errors){
+										sweetAlert("Une erreur est survenue", errors, "error");
+									});
+									i= lines.length;
+									$('#submit-pattern').prop('disabled',false);
+									$("#next-results").hide();
+								}else if (lines[i] == '<?>'){
+									progression = 1;
+								}else if (progression == 1){
+									clearInterval(watcher);
+									$('#submit-pattern').prop('disabled',false);
+									$("#next-results").show();
+									$('#progress-txt').text(lines[i] + '% du corpus parcourus');
+									i = lines.length;
+								}else{
+									$("#list-results").append('<li id="list-' + i + '"><a href="#" >' +  lines[i] + '</a></li>');
+									url = './data/' + id + '/' + lines[i];
+									$('#list-' + i).click({url:url,i:i},display_picture);
+								}
+							};
+							resultsNumber = line;
                 			previous = ajax.responseText;
             			}
         			}
@@ -52,7 +140,7 @@ function request_pattern(){
 }
 
 function display_picture(event){
-	$('#result-pic').attr('src',event.data.url);
+	$('#result-pic').attr('data',event.data.url);
 	$('#list-results li').removeClass('displayed');
 	$('#list-' + event.data.i).addClass('displayed');
 }
