6f8324567dac4672bdac78870eea40dd259673ac
achemard <achemard@7838e531-6607-4d57-9587-6c381814729c>
2015-04-13 14:14:50 +0000

    Dir test
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/semagramme/grew_web@8612 7838e531-6607-4d57-9587-6c381814729c

--- shorten.php
+++ shorten.php
@@ -1,22 +1,22 @@
 <?php
 
 if (in_array($_SERVER["REMOTE_ADDR"],array("127.0.0.1","::1"))) {
-      define("DIR", "/Users/guillaum/forge/semagramme/grew_web/data/");
+      $dire = "/Users/guillaum/forge/semagramme/grew_web/data/";
     }else{
-      define("DIR", "/data/semagramme/www/grew/data/");
+      $dire =  "/data/semagramme/www/grew/data/";
     }
-  
+
 if (isset($_POST['pattern'])) {
 	$alphabet = '23456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ-_';
     $base = 51; // strlen(self::ALPHABET)
   	$str = '';
 
-  	$num = count(scandir($dir)) - 1 ;
+  	$num = count(scandir($dire)) - 1 ;
     while ($num > 0) {
         $str = substr($alphabet, ($num % $base), 1) . $str;
         $num = floor($num / $base);
     }
-    $shorten = fopen($dir . $str ,"w");
+    $shorten = fopen($dire . $str ,"w");
 	fwrite($shorten, $_POST['pattern']);
 	fclose($shorten);
 
