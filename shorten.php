ff654bd86615f5366be31a930fbbae43ca9d7eb9
achemard <achemard@7838e531-6607-4d57-9587-6c381814729c>
2015-04-27 08:27:50 +0000

    Shorten modifications
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/semagramme/grew_web@8654 7838e531-6607-4d57-9587-6c381814729c

--- shorten.php
+++ shorten.php
@@ -1,9 +1,9 @@
 <?php
 
 if (in_array($_SERVER["REMOTE_ADDR"],array("127.0.0.1","::1"))) {
-      $dir = "/Users/guillaum/forge/semagramme/grew_web/data/";
+      $dir = "/Users/guillaum/forge/semagramme/grew_web/data/shorten";
     }else{
-      $dir =  "/data/semagramme/www/grew/data/";
+      $dir =  "/data/semagramme/www/grew/data/shorten";
     }
 
 if (isset($_POST['pattern'])) {
