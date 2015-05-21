c2734a7ed85cb9fbd9ce0cb6087a1e2ad0a618e4
bguillaum <bguillaum@7838e531-6607-4d57-9587-6c381814729c>
2015-05-21 10:40:49 +0000

    fix log files
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/semagramme/grew_web@8679 7838e531-6607-4d57-9587-6c381814729c

--- corpora/jenkins_script.sh
+++ corpora/jenkins_script.sh
@@ -24,8 +24,8 @@ cd ..
 cd uni-dep-tb-fr-2.0
 PORT=`cat port`  # 8183
 FILE=/data/semagramme/resources/uni-dep-tb-fr-2.0.conll
-LOG=/data/semagramme/log/grew_daemon_tiger.log
-ERROR_LOG=/data/semagramme/log/grew_daemon_tiger_error.log
+LOG=/data/semagramme/log/grew_daemon_uni-dep-tb-fr.log
+ERROR_LOG=/data/semagramme/log/grew_daemon_uni-dep-tb-fr_error.log
 echo "/home/guillaum/.opam/4.02.0/bin/grew_daemon -p ${PORT} -c ${FILE} -d ../uni-dep-tb-all-2.0.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
 cd ..
 
@@ -33,8 +33,8 @@ cd ..
 cd uni-dep-tb-fr-2.0
 PORT=`cat port`  # 8184
 FILE=/data/semagramme/resources/uni-dep-tb-ko-2.0.conll
-LOG=/data/semagramme/log/grew_daemon_tiger.log
-ERROR_LOG=/data/semagramme/log/grew_daemon_tiger_error.log
+LOG=/data/semagramme/log/grew_daemon_uni-dep-tb-ko.log
+ERROR_LOG=/data/semagramme/log/grew_daemon_uni-dep-tb-ko_error.log
 echo "/home/guillaum/.opam/4.02.0/bin/grew_daemon -p ${PORT} -c ${FILE} -d ../uni-dep-tb-all-2.0.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
 cd ..
 
