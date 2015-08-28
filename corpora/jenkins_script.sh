0ef26fca11cbac8e7854e521bbd1cd35058f212d
bguillaum <bguillaum@7838e531-6607-4d57-9587-6c381814729c>
2015-08-28 15:22:29 +0000

    fix ocaml version in jenkin script
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/semagramme/grew_web@8780 7838e531-6607-4d57-9587-6c381814729c

--- corpora/jenkins_script.sh
+++ corpora/jenkins_script.sh
@@ -2,13 +2,15 @@
 sudo killall grew_daemon || true
 sleep 5
 
+EXEC=/home/guillaum/.opam/last/bin/grew_daemon
+
 # sequoia
 cd sequoia-6.0
 PORT=`cat port` # 8181
 FILE=/data/semagramme/resources/sequoia-6.0.conll
 LOG=/data/semagramme/log/grew_daemon_sequoia.log
 ERROR_LOG=/data/semagramme/log/grew_daemon_sequoia_error.log
-echo "/home/guillaum/.opam/4.02.0/bin/grew_daemon -p ${PORT} -c ${FILE} -d ../sequoia_decl.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
+echo "${EXEC} -p ${PORT} -c ${FILE} -d ../sequoia_decl.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
 cd ..
 
 # deep-sequoia
@@ -17,7 +19,7 @@ PORT=`cat port` # 8182
 FILE=/data/semagramme/resources/deep-sequoia-1.1.conll
 LOG=/data/semagramme/log/grew_daemon_deep-sequoia.log
 ERROR_LOG=/data/semagramme/log/grew_daemon_deep-sequoia_error.log
-echo "/home/guillaum/.opam/4.02.0/bin/grew_daemon -p ${PORT} -c ${FILE} -d ../sequoia_decl.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
+echo "${EXEC} -p ${PORT} -c ${FILE} -d ../sequoia_decl.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
 cd ..
 
 # UDT 1.1- French
@@ -26,7 +28,7 @@ PORT=`cat port` # 8183
 FILE=/data/semagramme/resources/fr-ud-all.conll
 LOG=/data/semagramme/log/grew_daemon_UD.log
 ERROR_LOG=/data/semagramme/log/grew_daemon_UD_error.log
-echo "/home/guillaum/.opam/4.02.0/bin/grew_daemon -p ${PORT} -c ${FILE} -d ../UD.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
+echo "${EXEC} -p ${PORT} -c ${FILE} -d ../UD.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
 cd ..
 
 # UDT- English
@@ -35,7 +37,7 @@ PORT=`cat port` # 8184
 FILE=/data/semagramme/resources/en-ud-all.conll
 LOG=/data/semagramme/log/grew_daemon_UD.log
 ERROR_LOG=/data/semagramme/log/grew_daemon_UD_error.log
-echo "/home/guillaum/.opam/4.02.0/bin/grew_daemon -p ${PORT} -c ${FILE} -d ../UD.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
+echo "${EXEC} -p ${PORT} -c ${FILE} -d ../UD.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
 cd ..
 
 # Universal Treebank v2.0- Korean
@@ -45,7 +47,7 @@ FILE=/data/semagramme/resources/uni-dep-tb-ko-2.0.conll
 SPECIAL_CHARS=/data/semagramme/resources/ko_chars
 LOG=/data/semagramme/log/grew_daemon_uni-dep-tb-ko.log
 ERROR_LOG=/data/semagramme/log/grew_daemon_uni-dep-tb-ko_error.log
-echo "/home/guillaum/.opam/4.02.0/bin/grew_daemon --special_chars ${SPECIAL_CHARS} -p ${PORT} -c ${FILE} -d ../uni-dep-tb-all-2.0.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
+echo "${EXEC} --special_chars ${SPECIAL_CHARS} -p ${PORT} -c ${FILE} -d ../uni-dep-tb-all-2.0.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
 cd ..
 
 # TIGER
@@ -54,7 +56,7 @@ PORT=`cat port` # 8186
 FILE=/data/semagramme/resources/tiger-2.2.conll
 LOG=/data/semagramme/log/grew_daemon_tiger.log
 ERROR_LOG=/data/semagramme/log/grew_daemon_tiger_error.log
-echo "/home/guillaum/.opam/4.02.0/bin/grew_daemon -p ${PORT} -c ${FILE} -d tiger_decl.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
+echo "${EXEC} -p ${PORT} -c ${FILE} -d tiger_decl.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
 cd ..
 
 # deep-sequoia-trunk
@@ -63,6 +65,6 @@ PORT=`cat port` # 8187
 FILE=/data/semagramme/resources/deep-sequoia.conll
 LOG=/data/semagramme/log/grew_daemon_deep-sequoia.log
 ERROR_LOG=/data/semagramme/log/grew_daemon_deep-sequoia_error.log
-echo "/home/guillaum/.opam/4.02.0/bin/grew_daemon -p ${PORT} -c ${FILE} -d ../sequoia_decl.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
+echo "${EXEC} -p ${PORT} -c ${FILE} -d ../sequoia_decl.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
 cd ..
 
