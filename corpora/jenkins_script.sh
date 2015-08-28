41a97d3e6dc04c49280215a25d9cbd49c7b288d4
bguillaum <bguillaum@7838e531-6607-4d57-9587-6c381814729c>
2015-08-28 14:42:00 +0000

    update corpora
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/semagramme/grew_web@8778 7838e531-6607-4d57-9587-6c381814729c

--- corpora/jenkins_script.sh
+++ corpora/jenkins_script.sh
@@ -20,18 +20,27 @@ ERROR_LOG=/data/semagramme/log/grew_daemon_deep-sequoia_error.log
 echo "/home/guillaum/.opam/4.02.0/bin/grew_daemon -p ${PORT} -c ${FILE} -d ../sequoia_decl.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
 cd ..
 
-# Universal Treebank v2.0- French
-cd uni-dep-tb-fr-2.0
+# UDT 1.1- French
+cd UD_French
 PORT=`cat port` # 8183
-FILE=/data/semagramme/resources/uni-dep-tb-fr-2.0.conll
-LOG=/data/semagramme/log/grew_daemon_uni-dep-tb-fr.log
-ERROR_LOG=/data/semagramme/log/grew_daemon_uni-dep-tb-fr_error.log
-echo "/home/guillaum/.opam/4.02.0/bin/grew_daemon -p ${PORT} -c ${FILE} -d ../uni-dep-tb-all-2.0.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
+FILE=/data/semagramme/resources/fr-ud-all.conll
+LOG=/data/semagramme/log/grew_daemon_UD.log
+ERROR_LOG=/data/semagramme/log/grew_daemon_UD_error.log
+echo "/home/guillaum/.opam/4.02.0/bin/grew_daemon -p ${PORT} -c ${FILE} -d ../UD.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
+cd ..
+
+# UDT- English
+cd UD_English
+PORT=`cat port` # 8184
+FILE=/data/semagramme/resources/en-ud-all.conll
+LOG=/data/semagramme/log/grew_daemon_UD.log
+ERROR_LOG=/data/semagramme/log/grew_daemon_UD_error.log
+echo "/home/guillaum/.opam/4.02.0/bin/grew_daemon -p ${PORT} -c ${FILE} -d ../UD.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
 cd ..
 
 # Universal Treebank v2.0- Korean
 cd uni-dep-tb-ko-2.0
-PORT=`cat port` # 8184
+PORT=`cat port` # 8185
 FILE=/data/semagramme/resources/uni-dep-tb-ko-2.0.conll
 SPECIAL_CHARS=/data/semagramme/resources/ko_chars
 LOG=/data/semagramme/log/grew_daemon_uni-dep-tb-ko.log
@@ -41,7 +50,7 @@ cd ..
 
 # TIGER
 cd tiger-2.2
-PORT=`cat port` # 8185
+PORT=`cat port` # 8186
 FILE=/data/semagramme/resources/tiger-2.2.conll
 LOG=/data/semagramme/log/grew_daemon_tiger.log
 ERROR_LOG=/data/semagramme/log/grew_daemon_tiger_error.log
@@ -50,7 +59,7 @@ cd ..
 
 # deep-sequoia-trunk
 cd deep-sequoia-trunk
-PORT=`cat port` # 8186
+PORT=`cat port` # 8187
 FILE=/data/semagramme/resources/deep-sequoia.conll
 LOG=/data/semagramme/log/grew_daemon_deep-sequoia.log
 ERROR_LOG=/data/semagramme/log/grew_daemon_deep-sequoia_error.log
