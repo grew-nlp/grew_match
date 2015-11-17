bbf98de431cbd59c166788e69dc15fa92588c7b6
bguillaum <bguillaum@7838e531-6607-4d57-9587-6c381814729c>
2015-11-17 14:09:13 +0000

    Update to UD 1.2
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/semagramme/grew_web@8845 7838e531-6607-4d57-9587-6c381814729c

--- corpora/jenkins_script.sh
+++ corpora/jenkins_script.sh
@@ -22,32 +22,22 @@ ERROR_LOG=/data/semagramme/log/grew_daemon_deep-sequoia_error.log
 echo "${EXEC} -p ${PORT} -c ${FILE} -d ../sequoia_decl.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
 cd ..
 
-# UDT 1.1- French
+# UD_French version 1.2
 cd UD_French
 PORT=`cat port` # 8183
-FILE=/data/semagramme/resources/fr-ud-all.conll
+FILE=/data/semagramme/resources/fr-ud-all-1.2.conll
 LOG=/data/semagramme/log/grew_daemon_UD.log
 ERROR_LOG=/data/semagramme/log/grew_daemon_UD_error.log
 echo "${EXEC} -p ${PORT} -c ${FILE} -d ../UD.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
 cd ..
 
-# UDT- English
+# UD_English version 1.2
 cd UD_English
 PORT=`cat port` # 8184
-FILE=/data/semagramme/resources/en-ud-all.conll
+FILE=/data/semagramme/resources/en-ud-all-1.2.conll
 LOG=/data/semagramme/log/grew_daemon_UD.log
 ERROR_LOG=/data/semagramme/log/grew_daemon_UD_error.log
 echo "${EXEC} -p ${PORT} -c ${FILE} -d ../UD.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
-cd ..
-
-# Universal Treebank v2.0- Korean
-cd uni-dep-tb-ko-2.0
-PORT=`cat port` # 8185
-FILE=/data/semagramme/resources/uni-dep-tb-ko-2.0.conll
-SPECIAL_CHARS=/data/semagramme/resources/ko_chars
-LOG=/data/semagramme/log/grew_daemon_uni-dep-tb-ko.log
-ERROR_LOG=/data/semagramme/log/grew_daemon_uni-dep-tb-ko_error.log
-echo "${EXEC} --special_chars ${SPECIAL_CHARS} -p ${PORT} -c ${FILE} -d ../uni-dep-tb-all-2.0.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
 cd ..
 
 # TIGER
@@ -68,3 +58,11 @@ ERROR_LOG=/data/semagramme/log/grew_daemon_deep-sequoia_error.log
 echo "${EXEC} -p ${PORT} -c ${FILE} -d ../sequoia_decl.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
 cd ..
 
+# UD_French Trunk version
+cd UD_French
+PORT=`cat port` # 8188
+FILE=/data/semagramme/resources/fr-ud-all-trunk.conll
+LOG=/data/semagramme/log/grew_daemon_UD.log
+ERROR_LOG=/data/semagramme/log/grew_daemon_UD_error.log
+echo "${EXEC} -p ${PORT} -c ${FILE} -d ../UD.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
+cd ..
