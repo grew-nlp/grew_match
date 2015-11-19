bcf45bedc39fa1645083a1dd1a6065f03828e18b
bguillaum <bguillaum@7838e531-6607-4d57-9587-6c381814729c>
2015-11-19 15:13:37 +0000

    new seq arch
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/semagramme/grew_web@8846 7838e531-6607-4d57-9587-6c381814729c

--- corpora/jenkins_script.sh
+++ corpora/jenkins_script.sh
@@ -5,25 +5,34 @@ sleep 5
 EXEC=/home/guillaum/.opam/last/bin/grew_daemon
 
 # sequoia
-cd sequoia-7.0
+cd sequoia.surf-7.0
 PORT=`cat port` # 8181
-FILE=/data/semagramme/resources/sequoia-7.0.conll
-LOG=/data/semagramme/log/grew_daemon_sequoia.log
-ERROR_LOG=/data/semagramme/log/grew_daemon_sequoia_error.log
+FILE=/data/semagramme/resources/sequoia-7.0/sequoia.surf.conll
+LOG=/data/semagramme/log/grew_daemon_sequoia.surf.log
+ERROR_LOG=/data/semagramme/log/grew_daemon_sequoia.surf_error.log
 echo "${EXEC} -p ${PORT} -c ${FILE} -d ../sequoia_decl.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
 cd ..
 
 # deep-sequoia
-cd deep-sequoia-7.0
+cd sequoia.deep_and_surf-7.0
 PORT=`cat port` # 8182
-FILE=/data/semagramme/resources/deep-sequoia-7.0.conll
-LOG=/data/semagramme/log/grew_daemon_deep-sequoia.log
-ERROR_LOG=/data/semagramme/log/grew_daemon_deep-sequoia_error.log
+FILE=/data/semagramme/resources/sequoia-7.0/sequoia.deep_and_surf.conll
+LOG=/data/semagramme/log/grew_daemon_sequoia.deep_and_surf.log
+ERROR_LOG=/data/semagramme/log/grew_daemon_sequoia.deep_and_surf_error.log
+echo "${EXEC} -p ${PORT} -c ${FILE} -d ../sequoia_decl.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
+cd ..
+
+# deep-sequoia
+cd sequoia.deep-7.0
+PORT=`cat port` # 8182
+FILE=/data/semagramme/resources/sequoia-7.0/sequoia.deep.conll
+LOG=/data/semagramme/log/grew_daemon_sequoia.deep.log
+ERROR_LOG=/data/semagramme/log/grew_daemon_sequoia.deep_error.log
 echo "${EXEC} -p ${PORT} -c ${FILE} -d ../sequoia_decl.grs > ${LOG} 2> ${ERROR_LOG}" | at now +1 minute
 cd ..
 
 # UD_French version 1.2
-cd UD_French
+cd UD_French-1.2
 PORT=`cat port` # 8183
 FILE=/data/semagramme/resources/fr-ud-all-1.2.conll
 LOG=/data/semagramme/log/grew_daemon_UD.log
@@ -32,7 +41,7 @@ echo "${EXEC} -p ${PORT} -c ${FILE} -d ../UD.grs > ${LOG} 2> ${ERROR_LOG}" | at 
 cd ..
 
 # UD_English version 1.2
-cd UD_English
+cd UD_English-1.2
 PORT=`cat port` # 8184
 FILE=/data/semagramme/resources/en-ud-all-1.2.conll
 LOG=/data/semagramme/log/grew_daemon_UD.log
@@ -59,7 +68,7 @@ echo "${EXEC} -p ${PORT} -c ${FILE} -d ../sequoia_decl.grs > ${LOG} 2> ${ERROR_L
 cd ..
 
 # UD_French Trunk version
-cd UD_French
+cd UD_French-trunk
 PORT=`cat port` # 8188
 FILE=/data/semagramme/resources/fr-ud-all-trunk.conll
 LOG=/data/semagramme/log/grew_daemon_UD.log
