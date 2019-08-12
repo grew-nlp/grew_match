0c49e4caebfa23497ea2d7f6750f59094268708b
Bruno Guillaume <Bruno.Guillaume@loria.fr>
2019-08-12 11:05:10 +0200

    Fix install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ PORT=8888
 mkdir -p $DEST
 
 # Copy the files in the right place
-cp -rf * $DEST
+cp *.php *.xml *.html *.png $DEST
+cp -r corpora css fonts icon js tables tuto $DEST
 
 # build local folders for storing data
 cd $DEST
@@ -16,8 +17,8 @@ mkdir -p data/shorten
 chmod -R 777 data
 
 # update parameters in the code
-echo ajaxGrew.php | sed "s+@PORT@+${PORT}+" | sed "s+@DATADIR@+$DEST/data/+" > __tmp_file && mv __tmp_file ajaxGrew.php
-echo export.php | sed "s+@PORT@+${PORT}+" | sed "s+@DATADIR@+$DEST/data/+" > __tmp_file && mv __tmp_file export.php
-echo purge.php | sed "s+@DATADIR@+$DEST/data/+" > __tmp_file && mv __tmp_file purge.php
-echo shorten.php | sed "s+@DATADIR@+$DEST/data/+" > __tmp_file && mv __tmp_file shorten.php
+cat ajaxGrew.php | sed "s+@PORT@+${PORT}+" | sed "s+@DATADIR@+$DEST/data/+" > __tmp_file && mv __tmp_file ajaxGrew.php
+cat export.php | sed "s+@PORT@+${PORT}+" | sed "s+@DATADIR@+$DEST/data/+" > __tmp_file && mv __tmp_file export.php
+cat purge.php | sed "s+@DATADIR@+$DEST/data/+" > __tmp_file && mv __tmp_file purge.php
+cat shorten.php | sed "s+@DATADIR@+$DEST/data/+" > __tmp_file && mv __tmp_file shorten.php
 
