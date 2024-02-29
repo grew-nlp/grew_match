80315f597f96a11d79cf55dfd2e1791e62d14427
Bruno Guillaume <Bruno.Guillaume@loria.fr>
2024-02-29 08:09:49 +0100

    Tuwari and more aggrid tables

--- aggrid_tables/make_all.sh
+++ aggrid_tables/make_all.sh
@@ -6,10 +6,17 @@ CORPORA+=" SUD_French-Rhapsodie"
 CORPORA+=" SUD_French-ParisStories"
 CORPORA+=" SUD_French-Sequoia"
 CORPORA+=" SUD_Haitian_Creole-Autogramm"
-CORPORA+=" SUD_Hausa-NorthernAutogramm"
-CORPORA+=" SUD_Hausa-SouthernAutogramm"
 CORPORA+=" SUD_Naija-NSC"
 CORPORA+=" SUD_Zaar-Autogramm"
+
+CORPORA+=" SUD_Darija-Autogramm"
+CORPORA+=" SUD_Egyptian_Arabic-Autogramm"
+CORPORA+=" SUD_Gbaya-Autogramm"
+CORPORA+=" SUD_Hausa-NorthernAutogramm"
+CORPORA+=" SUD_Hausa-SouthernAutogramm"
+CORPORA+=" SUD_PS_2023"
+CORPORA+=" SUD_Tunisian_Arabic-NAxLAT"
+CORPORA+=" SUD_Tuwari-Autogramm"
 
 for i in $CORPORA
 do
