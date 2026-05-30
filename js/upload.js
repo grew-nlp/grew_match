68a230143b0f72babe41774e243a03b6f2fdc85a
Bruno Guillaume <Bruno.Guillaume@loria.fr>
2026-05-30 21:45:45 +0200

    improve upload page

--- js/upload.js
+++ js/upload.js
@@ -3,7 +3,7 @@
 let app = new Vue({
   el: '#app',
   data: {
-    schemas: ["UD", "SUD", "Parseme"],
+    schemas: ["None", "UD", "SUD", "Parseme"],
     current_schema: "UD",
     config: undefined,
     backend_server: "http://localhost:10024/",
@@ -13,29 +13,58 @@ let app = new Vue({
     nb_files:0,
     count_upload:0,
 
-    file_list: [],
+    file_array: [],
 
-    sizes: {"nb_trees":0,"nb_tokens":0},
+    desc: {},
     url: "",
     log: [],
 
     uploading: false,
-    upload_message: "",
+
+  },
+  computed: {
+    total_size: function () {
+      return this.file_array.reduce((acc, file) => acc + file.size, 0);
+    }
+  },
+
+  methods: {
+    humanFileSize(bytes) {
+      if (bytes === 0) return '0 B';
+      const units = ['B','KB','MB','GB','TB'];
+      const i = Math.floor(Math.log(bytes) / Math.log(1024));
+      const v = bytes / Math.pow(1024, i);
+      return (v % 1 === 0 ? v : v.toFixed(2)) + ' ' + units[i];
+    },
+
+    remove_file(name) {
+      console.log (name)
+      app.file_array.splice(name, 1);
+    }
+
   }
+
 });
 
 // ====================================================================================================
 $("#corpus_input").change(function(event) {
   const files = event.target.files;
-  app.file_list = files;
+  app.file_array = Array.from(files);
 })
 
 // ====================================================================================================
 async function build_corpus() {
-  const files = app.file_list;
+  app.uploading = true
+  const files = app.file_array;
   const data = { schema: app.current_schema };
-  const session_id = await generic_files('http://localhost:10024', 'new_corpus', files, data);
-  app.url = window.location.protocol + "//" + window.location.host + "?single=" + session_id
+  const response = await generic_files('http://localhost:10024', 'new_corpus', files, data);
+  if (response.session_id) {
+    app.url = window.location.protocol + "//" + window.location.host + "?single=" + response.session_id
+    app.desc = response.desc
+  } else {
+    app.url = ''
+  }
+  app.uploading = false
 }
 
 // ------------------------------------------------------------
@@ -76,7 +105,7 @@ async function generic_files(backend, service, files, data) {
     const result = await response.json();
 
     if (result.status === "ERROR") {
-      alert(JSON.stringify(result.message));
+      direct_error(JSON.stringify(result.message));
       return null;
     } else {
       return result.data;
